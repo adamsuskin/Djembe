b63c5e3e2b0b2aea9908162902a4db3b2c08ef81
Adam Suskin <adamsuskin@gmail.com>
2015-10-29 15:21:16 -0400

    Initial project

---
+++ Djembe/Djembe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Djembe
+//
+//  Created by Adam Suskin on 10/29/15.
+//  Copyright © 2015 Adam Suskin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
